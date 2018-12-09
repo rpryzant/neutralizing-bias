15c2b01f26edc71a9e2802d08e39b39239b07f2d
rpryzant <rpryzant@stanford.edu>
2018-12-09 12:30:41 +0900

    eval classifier

--- scripts/eval/run_eval.py
+++ scripts/eval/run_eval.py
@@ -5,14 +5,20 @@ run eval on a pair of corpus files
 import sys
 sys.path.append('../../src/style_transfer_baseline')
 import evaluation
+import models
 
 src_path = sys.argv[1]
 pred_path = sys.argv[2]
 tgt_path = sys.argv[3]
+classifier_path = "../../data/v2/eval_classifier"
+
+eval_classifier = models.TextClassifier.from_pickle(
+    "../../data/v2/eval_classifier")
+
 
 src = [x.strip().split() for x in open(src_path)]
 pred = [x.strip().split() for x in open(pred_path)]
 tgt = [x.strip().split() for x in open(tgt_path)]
 
-print(evaluation.get_metrics(src, pred, tgt))
+print(evaluation.get_metrics(src, pred, tgt, classifier=eval_classifier))
 
