add5f45b7e5fe68b379b6a940f57003cbb439139
rpryzant <rpryzant@stanford.edu>
2019-01-04 16:15:10 +0900

    more optimization

--- src/multi_disc_tagg/seq2seq_model.py
+++ src/multi_disc_tagg/seq2seq_model.py
@@ -285,9 +285,8 @@ class Seq2Seq(nn.Module):
             # run input through the model
             with torch.no_grad():
                 decoder_logit, word_probs = self.forward(pre_id, tgt_input, pre_mask, pre_len)
-            next_preds = torch.max(word_probs, dim=2)[1][:, -1]
+            next_preds = torch.max(word_probs[:, -1, :], dim=1)[1]
             tgt_input = torch.cat((tgt_input, next_preds.unsqueeze(1)), dim=1)
-
             # move to cpu because otherwise quickly runs out of mem
 #            out_logits.append(decoder_logit[:, -1, :].detach().cpu())
 
