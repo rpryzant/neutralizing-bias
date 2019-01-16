e8c81bb3cfff6b875aa6e58b1b4f5ee0a1fb8348
rpryzant <rpryzant@stanford.edu>
2019-01-16 14:39:57 +0900

    new seq2seq stuff

---
+++ src/multi_disc_tagg/seq2seq_test.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# python seq2seq.py --test ../../data/v5/final/bias.test --working_dir TEST/ --checkpoint TEST/model_1.ckpt
+
+from collections import defaultdict
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from tqdm import tqdm
+import os
+import torch
+from pytorch_pretrained_bert.tokenization import BertTokenizer
+from simplediff import diff
+import pickle
+from tensorboardX import SummaryWriter
+import torch.optim as optim
+import torch.nn as nn
+import numpy as np
+from collections import Counter
+import math
+
+from pytorch_pretrained_bert.modeling import BertEmbeddings
+from pytorch_pretrained_bert.optimization import BertAdam
+
+from data import get_dataloader
+import seq2seq_model
+from seq2seq_args import ARGS
+
+import seq2seq_utils as utils
+
+
+BERT_MODEL = "bert-base-uncased"
+
+# TODO REFACTER AWAY ALL THIS JUNK
+
+train_data_prefix = ARGS.train
+test_data_prefix = ARGS.test
+
+working_dir = ARGS.working_dir
+if not os.path.exists(working_dir):
+    os.makedirs(working_dir)
+
+TEST_TEXT = test_data_prefix + '.pre'
+TEST_TEXT_POST = test_data_prefix + '.post'
+
+WORKING_DIR = working_dir
+
+NUM_BIAS_LABELS = 2
+NUM_TOK_LABELS = 3
+
+if ARGS.bert_encoder:
+    TEST_BATCH_SIZE = 16
+else:
+    TEST_BATCH_SIZE = ARGS.batch_size // ARGS.beam_width
+
+MAX_SEQ_LEN = ARGS.max_seq_len
+
+CUDA = (torch.cuda.device_count() > 0)
+                                                                
+
+# # # # # # # # ## # # # ## # # DATA  # # # # # # # # ## # # # ## # #
+tokenizer = BertTokenizer.from_pretrained(BERT_MODEL, cache_dir=WORKING_DIR + '/cache')
+tok2id = tokenizer.vocab
+tok2id['<del>'] = len(tok2id)
+
+eval_dataloader, num_eval_examples = get_dataloader(
+    TEST_TEXT, TEST_TEXT_POST,
+    tok2id, TEST_BATCH_SIZE, MAX_SEQ_LEN, WORKING_DIR + '/test_data.pkl',
+    test=True, add_del_tok=ARGS.add_del_tok)
+    #tok_dists_path=ARGS.token_distribution_file)
+
+
+
+# # # # # # # # ## # # # ## # # MODELS # # # # # # # # ## # # # ## # #
+if ARGS.no_tok_enrich:
+    model = seq2seq_model.Seq2Seq(
+        vocab_size=len(tok2id), hidden_size=ARGS.hidden_size,
+        emb_dim=768, dropout=0.2, tok2id=tok2id)
+else:
+    model = seq2seq_model.Seq2SeqEnrich(
+        vocab_size=len(tok2id), hidden_size=ARGS.hidden_size,
+        emb_dim=768, dropout=0.2, tok2id=tok2id)
+
+model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+params = sum([np.prod(p.size()) for p in model_parameters])
+print('NUM PARAMS: ', params)
+
+if os.path.exists(ARGS.checkpoint):
+    print('LOADING FROM ' + ARGS.checkpoint)
+    model.load(ARGS.checkpoint)
+    print('...DONE')
+
+if CUDA:
+    model = model.cuda()
+
+
+# # # # # # # # # # # # EVAL # # # # # # # # # # # # # #
+print('EVAL...')
+model.eval()
+hits, preds, golds = utils.run_eval(
+    model, eval_dataloader, tok2id, ARGS.checkpoint + '.inference_results.txt',
+    MAX_SEQ_LEN, ARGS.beam_width)
+print('BLEU:\t\t ' + str(utils.get_bleu(preds, golds)))
+print('HITS:\t\t ' + str(np.mean(hits)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/multi_disc_tagg/seq2seq_utils.py
@@ -0,0 +1,161 @@
+import torch
+import math
+from collections import Counter
+import numpy as np
+import torch.nn as nn
+from tqdm import tqdm
+from simplediff import diff
+
+
+CUDA = (torch.cuda.device_count() > 0)
+
+
+
+
+def bleu_stats(hypothesis, reference):
+    """Compute statistics for BLEU."""
+    stats = []
+    stats.append(len(hypothesis))
+    stats.append(len(reference))
+    for n in range(1, 5):
+        s_ngrams = Counter(
+            [tuple(hypothesis[i:i + n]) for i in range(len(hypothesis) + 1 - n)]
+        )
+        r_ngrams = Counter(
+            [tuple(reference[i:i + n]) for i in range(len(reference) + 1 - n)]
+        )
+        stats.append(max([sum((s_ngrams & r_ngrams).values()), 0]))
+        stats.append(max([len(hypothesis) + 1 - n, 0]))
+    return stats
+
+def bleu(stats):
+    """Compute BLEU given n-gram statistics."""
+    if len(list(filter(lambda x: x == 0, stats))) > 0:
+        return 0
+    (c, r) = stats[:2]
+    log_bleu_prec = sum(
+        [math.log(float(x) / y) for x, y in zip(stats[2::2], stats[3::2])]
+    ) / 4.
+    return math.exp(min([0, 1 - float(r) / c]) + log_bleu_prec)
+
+def get_bleu(hypotheses, reference):
+    """Get validation BLEU score for dev set."""
+    stats = np.array([0., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
+    for hyp, ref in zip(hypotheses, reference):
+        stats += np.array(bleu_stats(hyp, ref))
+        return 100 * bleu(stats)
+
+
+def train_for_epoch(model, dataloader, tok2id, optimizer, loss_fn):
+    global CUDA
+
+    losses = []
+    for step, batch in enumerate(tqdm(dataloader)):
+        while True:
+            if CUDA:
+                batch = tuple(x.cuda() for x in batch)
+            (
+                pre_id, pre_mask, pre_len, 
+                post_in_id, post_out_id, 
+                pre_tok_label_id, post_tok_label_id, 
+                replace_id, _, _
+            ) = batch
+
+            post_logits, post_probs = model(pre_id, post_in_id, pre_mask, pre_len, pre_tok_label_id)
+            loss = loss_fn(post_logits, post_out_id, post_tok_label_id)
+            loss.backward()
+            norm = nn.utils.clip_grad_norm_(model.parameters(), 3.0)
+            optimizer.step()
+            model.zero_grad()
+
+            losses.append(loss.detach().cpu().numpy())
+
+    return losses
+
+
+def dump_outputs(src_ids, gold_ids, predicted_ids, gold_replace_id, gold_tok_dist, id2tok, out_file):
+    out_hits = []
+    preds_for_bleu = []
+    golds_for_bleu = []
+
+    for src_seq, gold_seq, pred_seq, gold_replace, gold_dist in zip(
+        src_ids, gold_ids, predicted_ids, gold_replace_id, gold_tok_dist):
+
+        src_seq = [id2tok[x] for x in src_seq]
+        gold_seq = [id2tok[x] for x in gold_seq]
+        pred_seq = [id2tok[x] for x in pred_seq[1:]]   # ignore start token
+        if '止' in gold_seq:
+            gold_seq = gold_seq[:gold_seq.index('止')]
+        if '止' in pred_seq:
+            pred_seq = pred_seq[:pred_seq.index('止')]
+        src_seq = ' '.join(src_seq).replace('[PAD]', '').strip()
+        gold_seq = ' '.join(gold_seq).replace('[PAD]', '').strip()
+        pred_seq = ' '.join(pred_seq).replace('[PAD]', '').strip()
+
+        gold_replace = id2tok[gold_replace]
+        pred_replace = [chunk for tag, chunk in diff(src_seq.split(), pred_seq.split()) if tag == '+']
+        try:
+            print('#' * 80, file=out_file)
+            print('IN SEQ: \t', src_seq.encode('utf-8'), file=out_file)
+            print('GOLD SEQ: \t', gold_seq.encode('utf-8'), file=out_file)
+            print('PRED SEQ:\t', pred_seq.encode('utf-8'), file=out_file)
+            print('GOLD DIST: \t', list(gold_dist), file=out_file)
+            print('GOLD TOK: \t', gold_replace.encode('utf-8'), file=out_file)
+            print('PRED TOK: \t', pred_replace, file=out_file)
+        except UnicodeEncodeError:
+            pass
+
+        if gold_seq == pred_seq:
+            out_hits.append(1)
+        else:
+            out_hits.append(0)
+
+        preds_for_bleu.append(pred_seq.split())
+        golds_for_bleu.append(gold_seq.split())
+
+    return out_hits, preds_for_bleu, golds_for_bleu
+
+
+def run_eval(model, dataloader, tok2id, out_file_path, max_seq_len, beam_width=1):
+    id2tok = {x: tok for (tok, x) in tok2id.items()}
+
+    weight_mask = torch.ones(len(tok2id))
+    weight_mask[0] = 0
+    criterion = nn.CrossEntropyLoss(weight=weight_mask)
+
+    out_file = open(out_file_path, 'w')
+
+    losses = []
+    hits = []
+    preds, golds = [], []
+    for step, batch in enumerate(tqdm(dataloader)):
+        if CUDA:
+            batch = tuple(x.cuda() for x in batch)
+        (
+            pre_id, pre_mask, pre_len, 
+            post_in_id, post_out_id, 
+            pre_tok_label_id, _, 
+            replace_id, _, _
+        ) = batch
+        
+        post_start_id = tok2id['行']
+        max_len = min(max_seq_len, pre_len[0].detach().cpu().numpy() + 10)
+
+        with torch.no_grad():
+            predicted_toks = model.inference_forward(
+                pre_id, post_start_id, pre_mask, pre_len, max_len, pre_tok_label_id,
+                beam_width=beam_width)
+
+        new_hits, new_preds, new_golds = dump_outputs(
+            pre_id.detach().cpu().numpy(), 
+            post_out_id.detach().cpu().numpy(), 
+            predicted_toks, 
+            replace_id.detach().cpu().numpy(), 
+            pre_tok_label_id.detach().cpu().numpy(), 
+            id2tok, out_file)
+        hits += new_hits
+        preds += new_preds
+        golds += new_golds
+    out_file.close()
+
+    return hits, preds, golds
