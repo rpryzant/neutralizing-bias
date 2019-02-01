e4e23491642a9aaa42f0876fef16c998d661f3d8
rpryzant <rpryzant@stanford.edu>
2019-02-01 10:54:32 +0900

    moving files aroudn

--- src/debiaser/stanford_parser/install_parser.sh
+++
@@ -1,6 +0,0 @@
-wget http://nlp.stanford.edu/software/stanford-parser-full-2015-12-09.zip
-unzip stanford-parser-full-2015-12-09.zip
-export STANFORDTOOLSDIR=$PWD
-export CLASSPATH=$STANFORDTOOLSDIR/stanford-parser-full-2015-12-09/stanford-parser.jar:$STANFORDTOOLSDIR/stanford-parser-full-2015-12-09/stanford-parser-3.6.0-models.jar
-
-
