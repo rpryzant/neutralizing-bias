bf4875e56e47e151eb82498c66657f21e1d885b9
rpryzant <rpryzant@stanford.edu>
2019-03-13 14:35:52 +0900

    rm punct-only changes

--- eval/utils.py
+++ eval/utils.py
@@ -1,6 +1,8 @@
 import hashlib
+from simplediff import diff
+import re
 
-
+# TODO -- THROW OUT EXAMPLES WITH ONLY PUNCT DIFFERENCES
 def parse_results_file(fp, ignore_unchanged=False):
     """
     args:
@@ -19,6 +21,12 @@ def parse_results_file(fp, ignore_unchanged=False):
     def is_complete(d):
         return 'src' in d and 'pred' in d and 'dist' in d
 
+    def punct_diff(a, b):
+        d = diff(a.split(), b.split())
+        changed_text = ''.join([
+            ''.join(chunk).strip() for tag, chunk in d if tag != '='])
+        return not re.search('[a-z]', changed_text)
+
     out = {}
     cur = {}
     for l in open(fp):
@@ -26,8 +34,12 @@ def parse_results_file(fp, ignore_unchanged=False):
         if '#########' in l and is_complete(cur):
             src_hash = hashlib.md5(cur['src']).hexdigest()
             pred_hash = hashlib.md5(cur['pred']).hexdigest()
-            if not (ignore_unchanged and src_hash == pred_hash):
+            if ignore_unchanged:
+                if not src_hash == pred_hash and not punct_diff(str(cur['src']), str(cur['pred'])):
+                    out[src_hash] = cur
+            else:
                 out[src_hash] = cur
+
             cur = {}
         elif 'IN SEQ' in l:
             cur['src'] = eval(l.split('\t')[-1])
